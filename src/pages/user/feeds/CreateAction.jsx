dcef8ea8f350c47303383051f4a95509e20a78e7
Mukhammadyunus <dr.mukhammadyunus@gmail.com>
2025-10-15 17:26:17 +0200

    create post page

--- src/pages/user/feeds/CreateAction.jsx
+++ src/pages/user/feeds/CreateAction.jsx
@@ -1,16 +1,23 @@
-import React from 'react'
-import { useParams } from 'react-router-dom'
+import React, { useEffect } from 'react'
+import { Route, Routes, useParams } from 'react-router-dom'
+import CreatePost from './CreatePost'
+
+const actions ={
+  post: CreatePost,
+
+}
 
 const CreateAction = () => {
-  const { action } = useParams()
+  const {action} = useParams()
+  
+  
   return (
-    <div className="p-4 md:p-6">
-      <h2 className="text-xl font-semibold mb-2">Create: {action}</h2>
-      <p className="text-neutral-600">This page is intentionally left blank for now.</p>
-    </div>
+    <>
+      {actions[action] && (
+        React.createElement(actions[action])
+      )}
+    </>
   )
 }
 
 export default CreateAction
-
-
