e300165a376017fadab27b1b3c55f7ba1ca866ca
Mukhammadyunus <dr.mukhammadyunus@gmail.com>
2025-10-31 11:58:13 +0100

    added isAuthenticated state to manage log in and public access

--- src/app/authSlice.js
+++ src/app/authSlice.js
@@ -1,6 +1,8 @@
 import { createSlice } from '@reduxjs/toolkit';
 
 const initialState = {
+  // For now i am going to give False as a value for isAuthenticated state, later on i will think about to manage thoroughly
+  isAuthenticated: false,
   access_token: null, // short-lived, memory only
   refresh_token: localStorage.getItem('refresh_token') || null,
 };
