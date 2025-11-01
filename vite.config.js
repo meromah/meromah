6792e6612bb45e543fbaff9c78b5d365d61da4b8
Mukhammadyunus <dr.mukhammadyunus@gmail.com>
2025-11-01 15:56:51 +0100

    Added backend proxy address

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
-
+const VITE_PROXY_TARGET = import.meta.env.VITE_PROXY_TARGET
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
@@ -10,7 +10,7 @@ export default defineConfig({
     port: 5173,
     proxy: {
       '/api': {
-        target: 'http://localhost:8000',
+        target: VITE_PROXY_TARGET,
         changeOrigin: true,
         secure: false,
       }
